2fab7aaaed64d8957098fc2b212a1df623aec5a8
infoWeHerzIT <137147893+infoWeHerzIT@users.noreply.github.com>
2026-06-24 17:43:29 +0200

    safe secrets

--- Templates/emailjs-config.js
+++ Templates/emailjs-config.js
@@ -9,7 +9,7 @@ const EMAILJS_SERVICE_ID  = 'service_6jnlzxd';
 const EMAILJS_TEMPLATE_ID_Erfolgreiche_Registrierung_fur_Nutzer = 'template_e42axd9';
 const EMAILJS_TEMPLATE_ID_Dankeschön_und_Bonus = 'template_3nqqrk3';
 
-const EMAILJS_REGISTRIERUNG_TEST_BCC         = 'sorin.ratiu@weherzit.de; vukinadriana@gmail.com';
+const EMAILJS_REGISTRIERUNG_TEST_BCC         = 'sorin.ratiu@weherzit.de';
 const EMAILJS_REGISTRIERUNG_BCC         = 'sorin.ratiu@weherzit.de; vukinadriana@gmail.com';
 const EMAILJS_EVENT_DETAILS_CHANGE_BCC         = '';
 
