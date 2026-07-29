799bd26fb0ede47ced266d7e85490af3a40aaf74
infoWeHerzIT <137147893+infoWeHerzIT@users.noreply.github.com>
2026-07-29 17:40:24 +0200

    .

---
+++ constants.js
@@ -0,0 +1,5 @@
+// ================================================================
+// Globale Konstanten
+// ================================================================
+
+const CC_NOTIFICATION_EMAILS = 'sorin.ratiu@weherzit.de;vukinadriana@gmail.com';
